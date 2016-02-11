e99be1198f155fcb619a6967972957602cc77cc9
kuro-kuris <cswart@outlook.com>
2016-02-11 19:05:27 +0000

    customer

--- warehouse.py
+++ warehouse.py
@@ -6,10 +6,19 @@ class Warehouse():
         this.y = y
         this.products = products
 
+class Customer():
+    def __init__(x, y, product_num, items):
+        this.x = x
+        this.y = y
+        this.product_num = product_num
+        this.items = items
+
 def distance((x1, y1), (x2, y2)):
     diff_x = x1 - x2
     diff_y = y1 - y2
     euclidean_distance = sqrt(diff_x * diff_x + diff_y * diff_y)
     dis = ceil(euclidean_distance)
     return dis
+
+
 
